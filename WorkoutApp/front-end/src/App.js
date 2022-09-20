a9b53f5f0ed90485176cd3bc2cfec2ba18e7da8e
TheAssembler1 <lewisnoah592@gmail.com>
2022-09-20 08:09:58 -0500

    added firebase auth and routing for the pages

--- WorkoutApp/front-end/src/App.js
+++ WorkoutApp/front-end/src/App.js
@@ -1,6 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./UserAuth/Login";
+import Register from "./UserAuth/Register";
+import MyProfile from "./Core/MyProfile";
+import Workouts from "./Core/Workouts";
+import MyWorkouts from "./Core/MyWorkouts";
+
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Workouts />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/myprofile" element={<MyProfile />} />
+          <Route exact path="/myworkouts" element={<MyWorkouts />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ WorkoutApp/front-end/src/Core/Workouts.js
@@ -0,0 +1,6 @@
+function Workouts() {
+    return <h1>Workouts</h1>;
+}
+
+
+export default Workouts;
