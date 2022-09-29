ef4b591af8637208c509a877cb5a635376db397c
Mohammad Rasool <ummaduddin29@gmail.com>
2022-09-29 16:30:53 -0500

    Changed NavBar

--- WorkoutApp/front-end/src/Core/Navbar.js
+++ WorkoutApp/front-end/src/Core/Navbar.js
@@ -6,6 +6,7 @@ class Navbar extends React.Component{
         constructor(props){
             super(props);
 
+
             this.state = {
                 workouts: [{"steps": [{}]}],
                 workoutsLoaded: true
@@ -33,17 +34,16 @@ class Navbar extends React.Component{
 
                 </div>
             )
-            
 
             return (
                 <nav>
                     <a href=""><img src="https://static.vecteezy.com/system/resources/previews/001/191/989/non_2x/circle-logo-png.png" alt="vader" className="nav--logo" /></a>
                     <h2 className='nav--logo_text'><a href="">Fit Happens</a></h2>
                     <ul className="nav--list">
-                        <li><a href="Home">Home</a></li>
-                        <li><a href="MyWorkouts">Your Workouts</a></li>
-                        <li><a href="MyProfile">Profile</a></li>
-                        <li><a href="Login">Log In</a></li>
+                        <li><a className = "nav--pages_text" href="Home">Home</a></li>
+                        <li><a className = "nav--pages_text" href="MyWorkouts">Your Workouts</a></li>
+                        <li><a className = "nav--pages_text" href="MyProfile">Profile</a></li>
+                        <li><a className = "nav--pages_text" href="Login">Log In</a></li>
                         <li className="sign--up"><a href="Register">Sign Up</a></li>
                     </ul>
                 </nav>
