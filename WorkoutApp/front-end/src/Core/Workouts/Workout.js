bd5369a23a48e625125d99d06a101d7531eea2b3
Bryce <boliv31@lsu.edu>
2022-10-18 15:45:14 -0500

    no box

--- WorkoutApp/front-end/src/Core/Workouts/Workout.js
+++ WorkoutApp/front-end/src/Core/Workouts/Workout.js
@@ -54,7 +54,7 @@ class Workout extends React.Component {
                                 </div>
                             ))
                         }
-                        <img class="gif" width="200" height="200" src={require(`../../Gifs/${workout.workoutType}.gif`)}></img>
+                        <img width="200" height="200" src={require(`../../Gifs/${workout.workoutType}.gif`)}></img>
                     </div>
                 </center>
             </div>
